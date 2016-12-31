b3c452447c07a0a847706705c002b0160feece8f
Cristina <cristinae.uni@gmail.com>
2016-12-31 11:09:37 +0100

    Introduction to the machine learning package. Trying with H20

---
+++ src/main/java/cat/lump/sts2017/ml/RandomForestH2O.java
@@ -0,0 +1,11 @@
+package cat.lump.sts2017.ml;
+
+/**
+ * Class to learn and apply a Random Forest on the STS data using the H2O library
+ * 
+ * @author cristina
+ * @since Dec 30, 2016
+ */
+public class RandomForestH2O {
+
+}
