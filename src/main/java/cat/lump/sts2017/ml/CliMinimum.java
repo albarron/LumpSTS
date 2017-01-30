99c778fb8ac4d82603a57bea6aa738af255bda17
Alberto Barron <albarron@gmail.com>
2017-01-30 15:01:40 +0300

    a simplified CLI for the classifiers

--- src/main/java/cat/lump/sts2017/ml/CliMinimum.java
+++ src/main/java/cat/lump/sts2017/ml/CliMinimum.java
@@ -78,48 +78,127 @@ public abstract class CliMinimum {
 			logger.error( "Unexpected exception: " + exp.getMessage() );			
 		}	
 		
-		if (!cLine.hasOption("f") && !cLine.hasOption("t") && !cLine.hasOption("m"))	{
-			logger.error("You need to specify training or test data");			
-		} 
-
 		if (cLine.hasOption("h")) {
-			formatter.printHelp(CliMinimum.class.getSimpleName(),options );
-			System.exit(0);
-		}
-		if (cLine.hasOption("f"))	{
-			training = cLine.getOptionValue("f");
-		} 
+      formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+      System.exit(0);
+    }
 
-		//if (cLine.hasOption("s"))	{
-		//	scores = new File(cLine.getOptionValue("s"));
-		//} 
+		boolean modelExists = false;
 
-		if (cLine.hasOption("t"))	{
-			test = cLine.getOptionValue("t");
-		} 
-
-		if (cLine.hasOption("m"))	{
-			model = cLine.getOptionValue("m");
-		} 
-
-		if (cLine.hasOption("f")  && !cLine.hasOption("t") && !cLine.hasOption("m"))	{
-			logger.warn("Training starting.");			
-		} 
-		
-		/*
-		if (cLine.hasOption("f") && !cLine.hasOption("s"))	{
-			logger.error("If you want to train the model you also need to give the scores.");			
-		} 
-		
-		if (!cLine.hasOption("f") && cLine.hasOption("s"))	{
-			logger.error("If you want to train the model you also need to give the features.");			
-		} 
-		*/
-		if (cLine.hasOption("f") && cLine.hasOption("t") && cLine.hasOption("m"))	{
-			logger.warn("You are giving a model, no training set needed. Proceeding with current model.");			
-		} else if (cLine.hasOption("t") && cLine.hasOption("m"))	{
-			logger.warn("Testing...");			
-		} 
+	  if (cLine.hasOption("m")) {
+      model = cLine.getOptionValue("m");
+      modelExists = (new File(model).isFile()) 
+          ? true
+          : false;
+    } 
+	  
+	  if (cLine.hasOption("f"))  {
+      training = cLine.getOptionValue("f");
+    }
+	  
+	  if (cLine.hasOption("t")) {
+      test = cLine.getOptionValue("t");
+    } 
+	  
+	  // if (cLine.hasOption("s")) {
+    //  scores = new File(cLine.getOptionValue("s"));
+    //} 
+	  
+	  if (training == null && test == null) {
+	    logger.error("No training nor test provided. Nothing to do"); 
+      formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+      System.exit(1);
+	  }
+	  
+	  if (model == null && training == null) {
+	    logger.error("No model nor training provided. Nothing to do"); 
+      formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+      System.exit(1);
+	  }
+	  
+	  if (training != null && modelExists) {
+	    logger.error("Training and model provided, but model exists. Nothing to do"); 
+      formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+      System.exit(1);
+	  }
+	  
+	  
+	   
+//	  if (model == null) {
+//	    if (training == null) {
+//	      logger.error("No model nor training provided. Nothing to do"); 
+//	      formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+//	      System.exit(1);
+//	    } else {
+//	      logger.error("Training provided (and no model); assuming CV");
+//	    }
+//	  } else {
+//	    if (modelExists) {
+//	      if (training != null) {
+//	        logger.error("I'm given training, but the model exists. Nothing to do"); 
+//	        formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+//	        System.exit(1);
+//	      } 
+//	      if (test == null) {
+//	        logger.error("I'm given the model, but no test. Nothing to do"); 
+//          formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+//          System.exit(1);
+//	      }
+//	      
+//	    } else {
+//	      
+//	      
+//	    }
+//	    
+//	    
+//	  }
+//	  
+//	  
+//	  if  (training == null && test == null) {
+//	    logger.error("Please provive train or test"); 
+//      formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+//      System.exit(1);
+//	  }
+//	  
+//	  // There is no model and no training was provided 
+//	  if (training == null) {
+//	    if (modelExists) {
+//	      if (test == null)  {
+//	        logger.error("I have a model, bun no test to apply it to");
+//	         formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+//	         System.exit(1);
+//	      } else {
+//	        logger.info("Model and test files set. Proceeding with the predictions.");  
+//	      }
+//	    }
+//	  } else {
+//      if (model == null) {
+//        logger.info("Training file set. Proceeding CV.");
+//      }
+//    }
+//	    logger.error("No model exists and no training data provided"); 
+//	    formatter.printHelp(CliMinimum.class.getSimpleName(),options );
+//      System.exit(1);
+//	    }
+//	  } else {	  
+//  		
+//
+//	  }
+//
+//	  //TODO I'd remove this info from here to whenever the training actually starts. 
+//		if (cLine.hasOption("f")  && !modelExists)	{
+//			logger.info("Training starting.");			
+//		} 
+//		
+//		/*
+//		if (cLine.hasOption("f") && !cLine.hasOption("s"))	{
+//			logger.error("If you want to train the model you also need to give the scores.");			
+//		} 
+//		
+//		if (!cLine.hasOption("f") && cLine.hasOption("s"))	{
+//			logger.error("If you want to train the model you also need to give the features.");			
+//		} 
+//		*/
 
 
 		return cLine;		
